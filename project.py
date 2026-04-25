e052bdf947e7bdc663912331204fc7edf1cbef4f
adhayaimakmak <adha4728@gmail.com>
2026-04-25 19:52:01 +0700

    แก้ไขให้คลาส EDAAnalyzer สามารถทำงานได้อย่างถูกต้องครบถ้วนสมบูรณ์และอัพเดบกราฟอันใหม่พร้อมกับลบฟังช์ชั่น manage_missing เนื่องจากไปทับกับ  SimpleImputer ในฟังช์ชั่น create_preprocessing_pipeline

--- project.py
+++ project.py
@@ -7,7 +7,11 @@ import seaborn as sns
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 class DataLoader:
     def __init__(self,data):
         try:
@@ -40,29 +44,42 @@ class DataLoader:
 class EDAAnalyzer:
     def __init__(self, data):
         self.df = data
-    
-    def manage_missing(self):
-        for column in self.df.columns:
-            if self.df[column].isnull().sum()>0:
-                if self.df[column].dtype in ['float64', 'int64']:
-                    self.df[column].fillna(self.df[column].median(), inplace=True)
-                else:
-                    self.df[column].fillna(self.df[column].mode()[0], inplace=True)
-            else:
-                continue
-        return self.df
         
-    def Categorize(self):
-        numeric_features = self.df.select_dtypes(include=['int64', 'float64']).columns
-        categorical_features = self.df.select_dtypes(include=['object']).columns
+    def split_features_target(self, target_column):
+        X = self.df.drop(columns=[target_column])
+        y = self.df[target_column]
+        return X, y 
+    
+    def create_new_feature(self, df):
+        df['rooms_per_household'] = df['total_rooms'] / df['households'].replace(0, np.nan)
+        df['bedrooms_per_room'] = df['total_bedrooms'] / df['total_rooms'].replace(0, np.nan)
+    
+        df = df.drop(columns=['total_rooms', 'total_bedrooms', 'population', 'households'])
+        return df
+        
+    def Categorize(self, df):
+        numeric_features = df.select_dtypes(include=['int64', 'float64']).columns
+        categorical_features = df.select_dtypes(include=['object', 'str']).columns
+
         return numeric_features, categorical_features
+    
+    def train_test_split_data(self,X,y,test_size=0.2,random_state=42):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+        return X_train, X_test, y_train, y_test
+    
+    def processed_fit_Transform(self, proprocessor, X_train, X_test):
+        X_train_processed = proprocessor.fit_transform(X_train)
+        X_test_processed = proprocessor.transform(X_test)
+        return X_train_processed, X_test_processed
     
     def create_preprocessing_pipeline(self, numeric_features, categorical_features):
         numeric_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='median')),
             ('scaler', StandardScaler())
         ])
         
         categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent')),
         ('onehot', OneHotEncoder(handle_unknown='ignore')) # แปลงข้อความเป็น One-Hot 
         ])
         
@@ -81,6 +98,13 @@ class EDAAnalyzer:
         plt.title('Correlation Heatmap')
         plt.savefig('graphs/correlation_heatmap.png')
         plt.show()
+        
+class  ModelTrainer:
+    def __int__(self, X_train, y_train, X_test, y_test):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
 
 
 def main():
@@ -97,8 +121,18 @@ def main():
     #ส่วนของการวิเคราะห์ EDA
     
     eda = EDAAnalyzer(data)
-    eda.manage_missing()
-    eda.plot_correlation_heatmap()
-
+    #eda.plot_correlation_heatmap()#สร้างกราฟความสัมพันธ์ระหว่างตัวแปรเชิงตัวเลขลบคอมเมนต์ออกเมื่อรันครั้งแรกและปิดเพื่อดูกราฟความสัมพันธ์ระหว่างตัวแปรเชิงตัวเลขในครั้งถัดไป
+    target_column = 'median_house_value'
+    X, y = eda.split_features_target(target_column)
+    X = eda.create_new_feature(X)
+    
+    X_train, X_test, y_train, y_test = eda.train_test_split_data(X,y)
+    
+    numeric_features,categorical_features = eda.Categorize(X)
+    
+    preprocessor = eda.create_preprocessing_pipeline(numeric_features, categorical_features)
+    print(preprocessor)
+    X_train_processed , X_test_processed = eda.processed_fit_Transform(preprocessor, X_train, X_test)
+    
 if __name__ == "__main__":
     main()
